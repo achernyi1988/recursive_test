456256e014701fafb14b195eb002f493bab91bb6
Alex <ote1188@gmail.com>
2019-09-27 13:53:39 +0300

    add first version

--- src/App.js
+++ src/App.js
@@ -1,26 +1,48 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+const continents = require("./continents_test")
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+function recursiveIteration(object, callback, dash) {
+    for (var property in object) {
+        //not necessary hasOwnProperty method since this json contains properties
+        if (object.hasOwnProperty(property)) {
+            if (typeof object[property] == "object"){
+                recursiveIteration(object[property], callback, dash );
+            }else{
+                //found a property which is not an object, check for your conditions here
+
+                if(callback(object, property, dash)){
+                    dash+="------"
+                }
+            }
+        }
+    }
+}
+
+class App extends React.Component {
+
+
+    componentDidMount(){
+        let dash="";
+        recursiveIteration(continents, ((object, property, dash)=>{
+
+            if(property === "name" && object[property]) {
+
+                console.log(dash + object[property]);
+                return true;
+            }
+
+            return false;
+        }), dash);
+    }
+
+    render(){
+        return (
+            <div className="App">
+                Hi
+            </div>
+        )
+    }
 }
 
 export default App;
